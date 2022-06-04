b7ebbb86c67275cd5f7513342b7f0dc97e3998e4
Scott Giese <scttgs0@gmail.com>
2022-06-04 11:02:04 -0500

    Updated make script

--- make_foenix.sh
+++ make_foenix.sh
@@ -1,3 +1,13 @@
+# convert colors within the header panel
+# sed -i 's/\xb0/\x78/g' images/header.raw
+# sed -i 's/\x1a/\x79/g' images/header.raw
+
+# convert colors within the footer panel
+# sed -i 's/\x10/\x7e/g' images/footer.raw
+# sed -i 's/\x22/\x7a/g' images/footer.raw
+# sed -i 's/\x8a/\x7d/g' images/footer.raw
+# sed -i 's/\x3a/\x7b/g' images/footer.raw
+# sed -i 's/\xd4/\x7c/g' images/footer.raw
 
 64tass  --m65816 \
         --long-address \
